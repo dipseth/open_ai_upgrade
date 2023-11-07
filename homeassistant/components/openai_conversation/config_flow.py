33e5e4aef413bd758d8246ac4b6ddb79a08fc827
dipseth <sethrivers@gmail.com>
2023-11-07 16:12:54 -0600

    Update config_flow.py

--- homeassistant/components/openai_conversation/config_flow.py
+++ homeassistant/components/openai_conversation/config_flow.py
@@ -59,7 +59,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> None:
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    .api_key = data[CONF_API_KEY]
+    openai.api_key = data[CONF_API_KEY]
     await hass.async_add_executor_job(partial(openai.models.list, request_timeout=10))
 
 
